f6e1631bc9b09d6545007a338ba5e6d12eb6a30f
Harvey <97197014+YerikAH@users.noreply.github.com>
2025-02-06 16:18:53 -0500

    feat: :sparkles: First pop version

--- arxatec-site/tailwind.config.mjs
+++ arxatec-site/tailwind.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        robotoSlab: ["RobotoSlab"],
+        sans: ["DM Sans Variable", ...defaultTheme.fontFamily.sans],
       },
     },
   },
