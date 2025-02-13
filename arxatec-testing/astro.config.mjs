6575b11551415e63cf6f2ece58a5b90ab8e841c3
Harvey <97197014+YerikAH@users.noreply.github.com>
2025-02-13 17:37:51 -0500

    feat: :sparkles: Added config by deploy in vercel

--- arxatec-testing/astro.config.mjs
+++ arxatec-testing/astro.config.mjs
@@ -4,9 +4,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
@@ -14,4 +17,6 @@ export default defineConfig({
       prefixDefaultLocale: true,
     },
   },
+
+  adapter: vercel(),
 });
