7dc45a0a6f8120acddd23305caac76a72611a272
Harvey <97197014+YerikAH@users.noreply.github.com>
2025-03-05 21:45:45 -0500

    feat: :iphone: Add responsive design

--- arxatec-site/src/components/pages/shared/qa-section/index.jsx
+++ arxatec-site/src/components/pages/shared/qa-section/index.jsx
@@ -14,13 +14,13 @@ const QaSection = ({ faqSection }) => {
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40 w-full">
         <div className="mx-auto w-full">
           <div className="bg-white p-6 rounded-lg shadow-lg">
-            <h2 className="text-base/7 font-semibold text-blue-600 text-left">
+            <h2 className="lg:text-base/7 text-sm font-semibold text-blue-600 text-left">
               {faqSection.tag}
             </h2>
-            <p className="mt-2 max-w-lg text-pretty text-4xl font-semibold tracking-tight text-gray-950 text-left">
+            <p className="mt-2 max-w-lg text-pretty lg:text-4xl text-2xl font-semibold tracking-tight text-gray-950 text-left">
               {faqSection.title}
             </p>
-            <p className="mt-6 max-w-2xl text-pretty text-left text-base font-medium text-gray-600">
+            <p className="lg:mt-6 mt-2 max-w-2xl text-pretty text-left text-sm lg:text-base font-medium text-gray-600">
               {faqSection.description}
             </p>
           </div>
@@ -35,7 +35,7 @@ const QaSection = ({ faqSection }) => {
                     onClick={() => toggleFAQ(index)}
                     aria-expanded={openIndex === index}
                   >
-                    <span className="text-base font-semibold">
+                    <span className="md:text-base text-sm font-semibold">
                       {faq.question}
                     </span>
                     <span className="ml-6 flex h-7 items-center">
@@ -76,7 +76,7 @@ const QaSection = ({ faqSection }) => {
                     openIndex === index ? "" : "hidden"
                   }`}
                 >
-                  <p className="text-base/7">{faq.answer}</p>
+                  <p className="md:text-base/7 text-sm">{faq.answer}</p>
                 </dd>
               </div>
             ))}
