2aee9c85393281a937301be94aa7cb1820ac0283
Harvey <97197014+YerikAH@users.noreply.github.com>
2025-04-24 22:01:32 -0500

    feat(prisma): :sparkles: Eliminated obsolete models and fields in Prisma's design, optimizing the data structure.

--- arxatec-service/src/config/redis/index.ts
+++ arxatec-service/src/config/redis/index.ts
@@ -4,6 +4,9 @@ dotenv.config();
 
 const redisClient = createClient({
   url: process.env.REDIS_URL || "redis://127.0.0.1:6379",
+  socket: {
+    connectTimeout: 10000,
+  },
 });
 
 export { redisClient };
